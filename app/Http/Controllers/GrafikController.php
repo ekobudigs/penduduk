37b47fbdb6c99c8f197e36fcc85488ee39f6988c
MOCH EKO BUDI SETIAWAN <gseqoo@gmail.com>
2022-07-18 22:58:45 +0700

    add template sb admin

---
+++ app/Http/Controllers/GrafikController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GrafikController extends Controller
+{
+    public function index()
+    {
+        return view('umur');
+    }
+}
